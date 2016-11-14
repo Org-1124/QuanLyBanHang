ef2d179d424652b3fc18767725d9bb6595658ce8
tungntdev <tungntdev@gmail.com>
2016-11-14 18:24:13 +0700

    Xong Form
    mới chỉ xong form thôi

--- QuanLyBanHang/Properties/Settings.Designer.cs
+++ QuanLyBanHang/Properties/Settings.Designer.cs
@@ -32,5 +32,16 @@ namespace QuanLyBanHang.Properties {
                 return ((string)(this["QuanLyBanHangConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-L8JMH5Q\\SQLEXPRESS;Initial Catalog=QuanLyBanHang;Integrated S" +
+            "ecurity=True")]
+        public string QuanLyBanHangConnectionString1 {
+            get {
+                return ((string)(this["QuanLyBanHangConnectionString1"]));
+            }
+        }
     }
 }
