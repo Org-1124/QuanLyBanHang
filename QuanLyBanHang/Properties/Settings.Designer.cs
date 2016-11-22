973c66cbb4b0f5f487863aedf25a10d8af94234d
lehaisonmath6 <lehaisonmath6@gmail.com>
2016-11-22 16:02:38 +0700

    ok

--- QuanLyBanHang/Properties/Settings.Designer.cs
+++ QuanLyBanHang/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace QuanLyBanHang.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -30,6 +30,17 @@ namespace QuanLyBanHang.Properties {
         public string QuanLyBanHangConnectionString {
             get {
                 return ((string)(this["QuanLyBanHangConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=TUANANH-PC\\SQLEXPRESS;Initial Catalog=QuanLyBanHang;Integrated Securi" +
+            "ty=True")]
+        public string QuanLyBanHangConnectionString1 {
+            get {
+                return ((string)(this["QuanLyBanHangConnectionString1"]));
             }
         }
     }
